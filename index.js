666206d4d868ffe42d9c6923a7647c9b2990e2c0
LanaAdekola <118976806+LanaAdekola@users.noreply.github.com>
2022-12-28 19:51:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let hs = document.getElementById('hs')
+let As = document.getElementById('As')
+
+let nn = 0
+let vount = 0
+let count = 0
+
+function h1 () {
+    count += 1
+    hs.textContent = count
+}
+function h2 () {
+    count += 2
+    hs.textContent = count
+}
+function h3 () {
+    count += 3
+    hs.textContent = count
+}
+function A1 () {
+    vount += 1
+    As.textContent = vount
+}
+function A2 () {
+    vount += 2
+    As.textContent = vount
+}
+function A3 () {
+    vount += 3
+    As.textContent = vount
+}
